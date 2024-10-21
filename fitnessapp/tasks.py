a1ecd0dee34b0420b7cf70a7d20110f1b2e5e247
kofiarkoh <kofiarkoh@gmail.com>
2024-10-21 16:44:18 -0400

    update task to send reminder to host and invitees

--- fitnessapp/tasks.py
+++ fitnessapp/tasks.py
@@ -1,10 +1,36 @@
 from celery import shared_task
 import logging
-
+from flask_mail import Message
+from flask import current_app
+from datetime import timedelta, datetime
 
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
 @shared_task()
 def send_task_reminder_email():
-    LOGGER.info('sending email')
+    
+    
+
+    # today's date in "YYYY-MM-DD" format
+    today_str = datetime.now().strftime("%Y-%m-%d")
+
+
+    events_today = current_app.mongo.db.events.find({
+        "date": today_str
+    })
+
+    events_list = list(events_today)
+
+
+    for event in events_list:
+        msg = Message(
+            subject="Hello",
+            sender=current_app.config['MAIL_USERNAME'],
+            recipients=[event['host'], event['invited_friend']],
+        )
+        
+        msg.body = f"Dear User, you have an upcoming event ({event['exercise']}) today at {event['start_time']}"
+        current_app.mail.send(msg)
+
+    
